d4611cf66dc4986fa0a11eff4c853de90d37d4a3
gebetto7 <s1321056@cce.kanagawa-it.ac.jp>
2016-10-13 18:09:18 +0900

    salary_calculation changes
    合計時間の上書きは出来るが、計算が合わない

--- admin/salary_calculation.php
+++ admin/salary_calculation.php
@@ -4,6 +4,11 @@
     $json = file_get_contents($last_url);
     $last = json_decode($json, true);
 
+    /*合計時間計算のためのオブジェクト*/
+    $sum_url = "../data/salary/salary1.json";
+    $json = file_get_contents($sum_url);
+    $sum = json_decode($json, true);
+
     for ($count = $last['day'] + 1; $count <= $last['day'] + 7; $count++){
 
         /*JSONデータ(シフト情報)の読み込み*/
@@ -15,20 +20,30 @@
         $staff_url = "../data/staff.json";
         $json = file_get_contents($staff_url);
         $staff_array = json_decode($json,true);
-        var_dump($staff_array);
 
-        /*勤務合計時間計算のための*/
+        /**/
         $salary_url = "";
-        echo $count . "------------------------------<br><br>";
-        for ($shift_count = 0; $shift_count < sizeof($shift_array['shift']); $shift_count++){
+        echo $count . "------------------------------<br><br>"; //日にちの表示
+
+        for ($shift_count = 0; $shift_count < sizeof($shift_array['shift']); $shift_count++){   //シフト表に入ってる人の数だけループ
             for ($staff_count = 0; $staff_count < sizeof($staff_array['staff']); $staff_count++){
+                /*スタッフ情報とシフト情報を一致させるためにnumberに該当するスタッフが参照できるまでループ*/
+
                 if ($shift_array['shift'][$shift_count]['number'] == $staff_array['staff'][$staff_count]['number']) {
                     $name = $staff_array['staff'][$staff_count]['name'];
                 }
             }
+
             $salary_sum = 0;
             $salary_sum = $salary_sum + ($shift_array['shift'][$shift_count]['max'] - $shift_array['shift'][$shift_count]['min']);
-            echo $name . "さんは" . $salary_sum . "時間働きました<br>";
+            $sum['salary'][0]['weekly_hours'] = $sum['salary'][0]['weekly_hours'] + $salary_sum;
+            echo $name . "さんは" . $salary_sum . "時間働いたので合計時間は" . $sum['salary'][0]['weekly_hours'] . "時間です。<br>";
         }
+
     }
+    /*合計時間のjsonファイルへの書き出し*/
+    $fjson = fopen("../data/salary/salary1.json", "w+b");
+    fwrite($fjson, json_encode($sum, JSON_UNESCAPED_UNICODE));
+    fclose($fjson);
+
 ?>
