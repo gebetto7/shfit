3e0782d98d2fef72ac9c9005290dcd214bdb27ba
gebetto7 <s1321056@cce.kanagawa-it.ac.jp>
2016-10-17 18:33:14 +0900

    createいじるよ

--- admin/shift_create.php
+++ admin/shift_create.php
@@ -15,7 +15,7 @@
     $staff_array = json_decode($json,true);
     
     /*JSONデータ(シフト情報)の読み込み*/
-    $shift_url = "../data/shift/shift.json";
+    $shift_url = "../data/shift/2016102shift.json";
     $json = file_get_contents($shift_url);
     $shift_array = json_decode($json,true);
 
